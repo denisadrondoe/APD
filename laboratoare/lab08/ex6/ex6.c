a94a63709b5efffda7b4db7a16e0af5eebf11975
Drondoe Denisa <denisa.drondoe@stud.acs.upb.ro>
2026-01-09 17:07:43 +0200

    lab8

--- laboratoare/lab08/ex6/ex6.c
+++ laboratoare/lab08/ex6/ex6.c
@@ -16,13 +16,21 @@ int main (int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &old_rank); // The current process ID / Rank.
 
     // Split the MPI_COMM_WORLD in small groups.
+    MPI_Comm_split(MPI_COMM_WORLD, old_rank / GROUP_SIZE, old_rank, &custom_group);
+    MPI_Comm_size(custom_group, &new_size); // Size of the new group.
+    MPI_Comm_rank(custom_group, &new_rank); // Rank in the new group.
 
     printf("Rank [%d] / size [%d] in MPI_COMM_WORLD and rank [%d] / size [%d] in custom group.\n",
             old_rank, old_size, new_rank, new_size);
 
     // Send the rank to the next process.
 
+
     // Receive the rank.
+    MPI_Sendrecv(&old_rank, 1, MPI_INT, (new_rank + 1) % new_size, 0,
+             &recv_rank, 1, MPI_INT, (new_rank - 1 + new_size) % new_size, 0,
+             custom_group, MPI_STATUS_IGNORE);
+
 
     printf("Process [%d] from group [%d] received [%d].\n", new_rank,
             old_rank / GROUP_SIZE, recv_rank);
